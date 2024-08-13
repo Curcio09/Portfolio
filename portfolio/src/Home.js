0f552a4b89a192cf213a946f365f322a9749b269
Palmer9Eldritch <christopherjamescurcio@gmail.com>
2024-08-13 12:28:19 -0400

    Home button styling

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Home.css';
+import placeImage from './place.png'; // Import the image here
 
 function Home() {
   return (
     <div className="home-container">
-      <div className="content">
-        <p className="description">
-          Home
-        </p>
+      <div className="home-text">
+        <h1>I'm Chris Curcio</h1>
+        <p>Front-end developer and UX/UI designer.</p>
+        <button>Contact</button>
+      </div>
+      <div className="home-image">
+        <img src={placeImage} alt="Chris Curcio" /> {/* Use the imported image */}
       </div>
     </div>
   );
