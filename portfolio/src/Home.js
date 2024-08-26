ce96ec646f73ae0bcca781180d3daaa9ee93fefa
Palmer9Eldritch <christopherjamescurcio@gmail.com>
2024-08-26 12:53:39 -0400

    Home component update

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import './Home.css';
-import placeImage from './place.png'; // Import the image here
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import placeImage from './place.png';
+import Ellipse from './Ellipse.svg';
+import project from './project.png';
 
 function Home() {
   return (
-    <div className="home-container">
-      <div className="home-text">
-        <h1>I'm Chris Curcio</h1>
-        <p>Front-end developer and UX/UI designer.</p>
-        <button>Contact</button>
+    <div className="homeGrid">
+      <div className="oneOne">
+        <p>
+          I'm <span className="highlight-name">Chris Curcio</span><br/>
+          Front-end developer and<br />
+          UX/UI designer.
+        </p>
+        {/* Link to Contact Page */}
+        <Link to="/contact">
+          <button>Contact</button>
+        </Link>
       </div>
-      <div className="home-image">
-        <img src={placeImage} alt="Chris Curcio" /> {/* Use the imported image */}
+      <div className="oneTwo">
+        <div className="image-container">
+          <img src={Ellipse} alt="Ellipse Background" className="ellipse" />
+          <img src={placeImage} alt="Chris Curcio" className="profile-image" />
+        </div>
+      </div>
+
+      <div className="twoOne">
+        <img src={project} alt="Project" />
+      </div>
+      <div className="twoTwo">
+        <p>Explore my projects built <br/> with React and JavaScript, <br/>
+          Featuring detailed Figma <br/> UX/UI designs.</p>
+        {/* Link to Projects Page */}
+        <Link to="/projects">
+          <button>Projects</button>
+        </Link>
       </div>
     </div>
   );
