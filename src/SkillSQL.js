840812b6742637eda6a4f54fa1941b805ceaf4a9
Curcio09 <christopherjamescurcio@gmail.com>
2024-10-30 13:05:28 -0400

    SQL component update

--- src/SkillSQL.js
+++ src/SkillSQL.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './SkillSQL.css';
-import SQLDatabase from './img/Python.png';
+import SQLDatabase from './img/Library.png';
 
 function SQLSkillPage() {
   return (
@@ -13,15 +13,15 @@ function SQLSkillPage() {
 
       {/* Projects Section */}
       <section className="sql-projects">
-        <h2>Projects</h2>
+        <h2 className="sql-projects-header">Projects</h2>
         <div className="sql-projects-grid">
           <Link to="/SQLDatabaseProject" className="sql-project-item">
             <div className="sql-project-content">
               <img src={SQLDatabase} alt="SQL Database Project" className="sql-project-image" />
-              <div className="overlay">
+              <div className="sql-overlay">
                 <div className="sql-project-info">
-                  <h3>SQL Database Project</h3>
-                  <p>A comprehensive database project showcasing complex queries, data manipulation, and relational schema design.</p>
+                  <h3>Library Management</h3>
+                  <p>A SQL-based system to manage books, authors, borrowers, and loans, showcasing relational database design and complex queries.</p>
                 </div>
               </div>
             </div>
@@ -30,9 +30,9 @@ function SQLSkillPage() {
       </section>
 
       {/* Back Button */}
-      <div className="back-button-container">
+      <div className="sql-back-button-container">
         <Link to="/about">
-          <button className="back-button">Back</button>
+          <button className="sql-back-button">Back</button>
         </Link>
       </div>
     </div>
